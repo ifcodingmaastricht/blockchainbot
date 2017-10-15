8d3a99f39836a8847cb7b72f5ff0f1ca4181fbd4
Sven <svenreulen@gmail.com>
2017-10-15 16:50:03 +0200

    add transection fee

--- simulator.py
+++ simulator.py
@@ -6,18 +6,25 @@ import time
 walletBitcoin = 100.0
 walletDollar = 100000.0
 firstrun = True
-currentPrice = 0
+transection = 0.002
+
 print ('Starting simulator...')
 def getMidPrice():
     with urllib.request.urlopen('https://api.bitfinex.com/v1/pubticker/btcusd') as response:
         html = response.read()
     return float(json.loads(html)['mid'])
-oldPrice = getMidPrice()
+oldPrice = currentPrice = initPrice = getMidPrice()
+
+
+def getTransectionfee():
+    return transection * currentPrice
+
 
 def getTotalNetWorth():
     return walletDollar + (walletBitcoin * currentPrice)
 
 def showTotalNetWorth():
+    print ('Total bitcoin:', walletBitcoin, 'walletDollar', walletDollar)
     print ('Total networth:', getTotalNetWorth(), 'profit', getTotalNetWorth() - totalNetWorthStart )
 
 totalNetWorthStart = getTotalNetWorth()
@@ -28,12 +35,12 @@ while True:
     if currentPrice > oldPrice:
         print ("sell")
         walletBitcoin = walletBitcoin - 1
-        walletDollar = walletDollar + currentPrice
+        walletDollar = walletDollar + currentPrice - getTransectionfee()
         
     elif currentPrice < oldPrice:
         print ("buy")
         walletBitcoin = walletBitcoin + 1
-        walletDollar = walletDollar - currentPrice
+        walletDollar = walletDollar - currentPrice - getTransectionfee()
     showTotalNetWorth()
     
     oldPrice = currentPrice
