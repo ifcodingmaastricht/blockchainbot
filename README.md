8de68ec627a39d3f1df0bd89a258d5031a96d33a
Niels van Reijmersdal <niels@vanreijmersdal.nl>
2017-10-29 15:05:23 +0100

    Moved the database configuration to a config file

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # blockchainbot
 
 Project to simulate bitcoin buying and selling
+
+# packages used
+
+pip install pyyaml
+pip install psycopg2
--- database.py
+++ database.py
@@ -1,17 +1,26 @@
 import psycopg2
+import yaml
+
 
 class Database:
     cursor = None
     connection = None
+    config = yaml.safe_load(open("config.yml"))
 
     def connect(self):
         try:
-            self.connection = psycopg2.connect("dbname='cryptocurrencies' user='postgres' host='localhost' password='postgres'")
+            connection_string = "dbname='cryptocurrencies' "
+            connection_string += "user='" + self.config['database']['username'] + "' "
+            connection_string += "host='localhost' "
+            connection_string += "password='" + self.config['database']['password'] + "'"
+
+            self.connection = psycopg2.connect(connection_string)
             self.connection.autocommit = True
             self.cursor = self.connection.cursor()
-        except Exception as e: print(e)
+        except Exception as e:
+            print(e)
 
     def store(self, mid, bid, ask, last, low, high, volume, timestamp):
         self.cursor.execute("""INSERT INTO bitcoin.bitfinex VALUES(%s, %s, %s, %s, %s, %s, %s, %s);""",
-                    [mid, bid,ask, last,low, high, volume, timestamp])
-    
+                            [mid, bid, ask, last, low, high, volume, timestamp])
+
